2e95c85c622c420a3c12edc6e992bf2a964944d6
Hope <johnhope354@gmail.com>
2021-05-22 11:04:43 -0500

    few updates

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-var requestUrl = 'https://api.tomtom.com/map/1/tile/basic/main/1/5/6.jpg?key=DaDPG4cvKblbs1fR2GGIqx7iApLYCGEv&tileSize=256&view=Unified&language=en-US';
+var requestUrl = 'http://www.mapquestapi.com/directions/v2/route?key=kAuLKYebMSAVKTRJlvyqYwLhARo2v9lS&from=Clarendon Blvd,Arlington,VA&to=2400+S+Glebe+Rd,+Arlington,+VA';
 
-
-// JQuery AJAX
-$.ajax({
-  url: requestUrl,
-  method: 'GET',
-}).then(function (response) {
-  console.log('AJAX Response \n-------------');
-  console.log(response);
-});
 
 // Browser Fetch Method
-fetch(requestUrl, {mode:"no-cors"})
+//fetch(requestUrl)
   .then(function (response) {
     return response.json();
   })
@@ -19,14 +10,3 @@ fetch(requestUrl, {mode:"no-cors"})
     console.log('Fetch Response \n-------------');
     console.log(data);
   });
-
-// Browser XMLHttpRequest
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === XMLHttpRequest.DONE) {
-    console.log('XMLHttpRequest Response \n-------------');
-    console.log(xhr.response);
-  }
-};
-xhr.open('GET', requestUrl);
-xhr.send();
